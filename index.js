355dd2ce0f1bed383580219063ee317c2295a8ab
John Lundy <johnclundyy@gmail.com>
2023-12-19 16:13:06 -0700

    Created index with skeleton code

---
+++ index.js
@@ -0,0 +1,37 @@
+const healthPath = "/health";
+const registerPath = "/register";
+const loginPath = "/login";
+const verifyPath = "/verify";
+
+export const handler = async (event) => {
+  console.log("Request Event: ", event);
+  let response;
+  switch (true) {
+    case event.httpMethod === "GET" && event.path === healthPath:
+      response = buildResponse(200);
+      break;
+    case event.httpMethod === "POST" && event.path === registerPath:
+      response = buildResponse(200);
+      break;
+    case event.httpMethod === "POST" && event.path === loginPath:
+      response = buildResponse(200);
+      break;
+    case event.httpMethod === "POST" && event.path === verifyPath:
+      response = buildResponse(200);
+      break;
+    default:
+      response = buildResponse(400, "404 Not Found");
+  }
+  return response;
+};
+
+function buildResponse(statusCode, body) {
+  return {
+    statusCode: statusCode,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  };
+}
