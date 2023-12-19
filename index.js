1a664b0f717ddc1678e70a35f9d220862ce4b188
John Lundy <johnclundyy@gmail.com>
2023-12-19 16:21:00 -0700

    Added register event to index

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const registerService = require("./service/register");
+const loginService = require("./service/login");
+const verifyService = require("./service/verify");
 const healthPath = "/health";
 const registerPath = "/register";
 const loginPath = "/login";
@@ -11,7 +14,8 @@ export const handler = async (event) => {
       response = buildResponse(200);
       break;
     case event.httpMethod === "POST" && event.path === registerPath:
-      response = buildResponse(200);
+      const registerBody = JSON.parse(event.body);
+      response = await registerService.register(registerBody);
       break;
     case event.httpMethod === "POST" && event.path === loginPath:
       response = buildResponse(200);
