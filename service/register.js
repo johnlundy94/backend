e3f13282ddc2c05fa1ba830ff6d7dbe2ed945516
John Lundy <johnclundyy@gmail.com>
2023-12-20 11:27:35 -0700

    Completed the register file

--- service/register.js
+++ service/register.js
@@ -2,6 +2,9 @@ const AWS = require("aws-sdk");
 AWS.configure.update({
   region: "us-east-1",
 });
+const util = require("../utils/util");
+const bcrypt = require("bcryptjs");
+
 const dynamodb = new AWS.DynamoBD.DocumentClient();
 const userTable = "fitness-app-users";
 
@@ -11,6 +14,68 @@ async function register(userInfo) {
   const username = userInfo.username;
   const password = userInfo.password;
   if (!username || !name || !email || !password) {
-    return;
+    return util.buildResponse(401, {
+      message: "All fields are required",
+    });
   }
+
+  const dynamoUser = await getUser(username.toLowerCase().trim());
+  if (dynamoUser && dynamoUser.username) {
+    return util.buildResponse(401, {
+      message: "Username already exists",
+    });
+  }
+
+  const encryptedPW = bcrypt.hashSync(password.trim(), 10);
+  const user = {
+    name: name,
+    email: email,
+    username: username.toLowerCase().trim(),
+    password: encryptedPW,
+  };
+
+  const saveUserReponse = await saveUser(user);
+  if (!saveUserReponse) {
+    return util.buildResponse(503, { message: "Server Error" });
+  }
+
+  return util.buildResponse(200, { username: username });
 }
+
+async function getUser(username) {
+  const params = {
+    TableName: userTable,
+    Key: {
+      username: username,
+    },
+  };
+
+  return await dynamodb
+    .get(params)
+    .promise()
+    .then(
+      (response) => {
+        return response.Item;
+      },
+      (error) => {
+        console.error("Error getting user: ", error);
+      }
+    );
+}
+
+async function saveUser(user) {
+  const params = {
+    TableName: userTable,
+    Item: user,
+  };
+  return await dynamodb.put(params).promise.then(
+    () => {
+      return true;
+    },
+    (error) => {
+      console.error("Error saving user: ", error);
+    }
+  );
+}
+
+module.exports.register = register;
